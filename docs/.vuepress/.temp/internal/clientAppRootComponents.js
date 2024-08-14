6e7ef86961f69f49966705ac47fd0020ab2ddfdc
zc7355608 <2273705254@qq.com>
2024-08-14 20:32:26 +0800

    项目初始化

---
+++ docs/.vuepress/.temp/internal/clientAppRootComponents.js
@@ -0,0 +1,5 @@
+import clientAppRootComponent0 from 'C:/Users/22737/Desktop/webpack_docs/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
+
+export const clientAppRootComponents = [
+  clientAppRootComponent0,
+]
